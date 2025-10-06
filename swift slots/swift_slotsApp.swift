1b07bf55a692c04c22e8101cf6d586a4e9b770bc
John Garza <john.garza100@student.tsc.edu>
2025-10-06 09:38:36 -0500

    Initial Commit

---
+++ swift slots/swift_slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_slotsApp.swift
+//  swift slots
+//
+//  Created by John A. Garza on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift slotsTests/swift_slotsTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_slotsTests.swift
+//  swift slotsTests
+//
+//  Created by John A. Garza on 10/6/25.
+//
+
+import Testing
+@testable import swift_slots
+
+struct swift_slotsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
